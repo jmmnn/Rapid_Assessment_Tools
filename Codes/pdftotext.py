21ecc0186a4c0ddee04b24d6ec6bf3ab1e1bf633
higgs54 <maxwelllee54@gmail.com>
2017-01-18 10:59:42 -0500

    update

--- Codes/pdftotext.py
+++ Codes/pdftotext.py
@@ -2,14 +2,25 @@ import tempfile, subprocess, os
 
 class pdf_to_text():
 
-    def __init__(self, root_path):
+    def __init__(self, root_path, style = '-layout'):
+        '''
+        The class function will convert all pdf files within the directory into text files
+        :param root_path: input the root path for all pdf files
+        :param style:
+
+        -layout(default)     : maintain original physical layout
+        -table               : similar to -layout, but optimized for tables
+        -lineprinter         : use strict fixed-pitch/height layout
+        -raw                 : keep strings in content stream order
+        '''
+
         self.root_path = root_path
+        self.style = style
         self.files = []
 
     def find_all(self):
 
         dir_list = os.walk(self.root_path)
-        file_path = []
 
         for root, dirs, files in dir_list:
             for file in files:
@@ -24,16 +35,17 @@ class pdf_to_text():
             output = tempfile.NamedTemporaryFile()
             output_file = file.replace('.pdf', '.txt')
 
-            out, err = subprocess.Popen(['pdftotext', '-layout', file, output.name]).communicate()
+            out, err = subprocess.Popen(['pdftotext', self.style, file, output.name]).communicate()
 
             pdf = output.read()
 
             pdf = pdf.decode('windows-1252')
+
             if not os.path.isfile(file):
                 with open(output_file, 'w') as f:
                     f.write(pdf)
 
+if __name__ == '__main__':
 
-root_path = '/Users/maxwelllee54/GitHubs/Rapid_Assessment_Tools/SharedFiles/Fordham'
-
-pdf = pdf_to_text(root_path).convert()
+    root_path = '/Users/maxwelllee54/GitHubs/Rapid_Assessment_Tools/SharedFiles/Fordham'
+    pdf_to_text(root_path).convert()
